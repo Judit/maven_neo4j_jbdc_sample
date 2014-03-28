af0ce867eddc04efdd5b0efdd4320f977958b09d
Judit <judit.sarmiento@aentos.es>
2014-03-28 14:35:22 +0000

    Move DB connection code into DAO constructor

--- src/main/java/com/jdbc/DAO.java
+++ src/main/java/com/jdbc/DAO.java
@@ -9,28 +9,26 @@ import java.util.Properties;
 
 public class DAO{
 
-  public DAO() {
-  }
+  protected Neo4jConnection dbConnection;
 
-  protected ResultSet rs;
-
-  public ResultSet getFirstNode () {
+  public DAO() throws SQLException {
     final Driver driver = new Driver();
     final String hostPort = System.getProperty("graphenedb.url");
-    final String query = "START root=node(0) RETURN root";
     final Properties props = new Properties();
 
     props.put("user", System.getProperty("graphenedb.user"));
     props.put("password", System.getProperty("graphenedb.password"));
     props.put("legacy", ""); // Need to work with Neo4j Community Edition 2.0.1
 
-    try {
-      Neo4jConnection conn = driver.connect("jdbc:neo4j://" + hostPort, props);
-      rs = conn.createStatement().executeQuery(query);
-      rs.next();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    dbConnection = driver.connect("jdbc:neo4j://" + hostPort, props);
+  }
+
+
+  public ResultSet getFirstNode () throws SQLException {
+    final String query = "START root=node(0) RETURN root";
+
+    ResultSet rs = dbConnection.createStatement().executeQuery(query);
+    rs.next();
     return rs;
   }
 }
